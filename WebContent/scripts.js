10ad715d273d8378af880cd8843ebac01066fcf3
Stephan Heinemann <stephan.heinemann@hotmail.com>
2015-07-11 15:02:05 -0700

    changed form encoding script

--- WebContent/scripts.js
+++ WebContent/scripts.js
@@ -1,11 +1,18 @@
 <script type="text/javascript">
 var request = new XMLHttpRequest();
 
-function submitForm(method, url, formId) {
-	var form = document.getElementById(formId);
+function submitForm(method, url, fields) {
 	var mimeType = "application/x-www-form-urlencoded";
 	request.open(method, url);
 	request.setRequestHeader("Content-Type", mimeType);
-	request.send(new FormData(form));
+	request.send(encodeFields(fields));
+}
+
+function encodeFields(fields) {
+	var values = [];
+	for (var field of fields) {
+		values.push(encodeURIComponent(field) + '=' + encodeURIComponent(document.getElementById(field).value));
+	}
+	return values.join('&').replace(/%20/g, '+');
 }
 </script>
